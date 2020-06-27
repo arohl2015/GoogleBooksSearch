3382a77ed572063c7952055d50c1884a69cb12b9
Michael <mikeystoddard@gmail.com>
2020-06-27 11:33:50 -0700

    first add for saved and search

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 // This file exports the Input, TextArea, and FormBtn components
 
-export function Input(props) {
+export function Form(props) {
   return (
     <div className="form-group">
       <input className="form-control" {...props} />
@@ -10,18 +10,4 @@ export function Input(props) {
   );
 }
 
-export function TextArea(props) {
-  return (
-    <div className="form-group">
-      <textarea className="form-control" rows="20" {...props} />
-    </div>
-  );
-}
-
-export function FormBtn(props) {
-  return (
-    <button {...props} style={{ float: "right", marginBottom: 10 }} className="btn btn-success">
-      {props.children}
-    </button>
-  );
-}
+export default Form;
---
+++ client/src/pages/Saved.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import Nav from "../components/Nav";
+import Jumbotron from "../components/Jumbotron";
+import Wrapper from "../components/Wrapper";
+import { Container} from "../components/Grid";
+class Saved extends Component {
+  state = {
+    savedBooks: [],
+  };
+  componentDidMount() {
+    API.getSavedBooks()
+      .then(({ data }) => this.setState({ savedBooks: data }))
+      .catch((err) => console.log(err));
+  }
+  deleteBook = (index) => {
+    API.deleteBook(index)
+      .then((res) => {
+        console.log(res);
+        this.componentDidMount();
+      })
+      .catch((err) => console.log(err));
+  };
+  render() {
+    return (
+      <div>
+        <Jumbotron>
+          <Nav />
+        </Jumbotron>
+        <Wrapper>
+          <div style={{ padding: "25px" }}>
+            {this.state.savedBooks.map((book) => (
+              <Container
+                sender="Saved"
+                title={book.title}
+                authors={book.authors}
+                description={book.description}
+                image={book.image}
+                link={book.infoLink}
+                deleteBook={this.deleteBook}
+              ></Container>
+            ))}
+          </div>
+        </Wrapper>
+      </div>
+    );
+  }
+}
+export default Saved;
