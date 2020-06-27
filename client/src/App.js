7eac3dce1f9640c21888f19b977ef22d12147c9b
A Rohl <april.rohl2015@gmail.com>
2020-06-27 09:02:17 -0700

    updates to file/folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Nav from "./components/Nav"
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+import Jumbotron from "./components/Jumbotron";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+          <Nav />
+          <Jumbotron />
+          <Switch>
+            {/* renders the Search page when "/" route is hit */}
+            <Route exact path="/" component={Search} />
+            {/* renders the Search page when "/search" route is hit */}
+            <Route exact path="/search" component={Search} />
+            {/* renders the Saved page when "/saved" route is hit */}
+            <Route exact path="/saved" component={Saved} />
+          </Switch>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
 const bookRoutes = require("./books");
-const bookSearch = require("./search");
 
 // Book routes
 router.use("/books", bookRoutes);
-router.use("/search", bookSearch);
 
 module.exports = router;
